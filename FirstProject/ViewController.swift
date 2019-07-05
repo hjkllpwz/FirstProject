7a676c8db707ae314cb68862f0af8cf44577d4b0
AlanKuang <54323533@qq.com>
2019-07-05 10:25:06 +0800

    Initial Commit

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by JiaMin Kuang on 2019/7/5.
+//  Copyright © 2019年 JiaMin Kuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
